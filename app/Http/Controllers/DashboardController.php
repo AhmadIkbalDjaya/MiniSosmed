51d66ccf72b8bc1bb9ee56a4362c0f0336de11d8
AhmadIkbalDjaya <ikbaldjaya@gmail.com>
2022-06-18 14:14:32 +0700

    Memperbaiki postingan yang muncul pada dashboard agar yg munculhanya postingan kita dan postingan orang yg kita follow

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -5,14 +5,18 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Post;
 use App\Models\User;
-
+use Illuminate\Support\Facades\Auth;
 
 class DashboardController extends Controller
 {
     public function index(){
+        $following = Auth::user()->follows->pluck('id');
         return view('dashboard', [
             "title" => "Minsos",
-            "posts" => Post::latest()->get(),
+            // "posts" => Post::latest()->get(),
+            "posts" => Post::whereIn('user_id', $following)
+                            ->orWhere('user_id', Auth::user()->id)
+                            ->latest()->get(),
             "users" => User::all()
         ]);
     }
