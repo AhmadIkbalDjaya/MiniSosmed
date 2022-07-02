1aee3afd6332d7ecdfdc1f3001322f989075c4d4
AhmadIkbalDjaya <ikbaldjaya@gmail.com>
2022-07-02 14:51:36 +0700

    Memperbaiki tampilan halaman login dan registrasi

--- resources/views/register/index.blade.php
+++ resources/views/register/index.blade.php
@@ -1,43 +1,35 @@
 @extends('layouts.main')
 
 @section('main')
-<div class="row justify-content-center mt-5">
-    <div class="col-lg-5">
-        <main class="form-registration">
-            <h1 class="h3 mb-3 fw-normal text-center">Registrasion Form</h1>
-            <form action="/register" method="post">
-                @csrf
-                <div class="form-floating">
-                    <input type="text" name="name" class="form-control rounded-top @error('name') is-invalid @enderror" id="name" placeholder="Name" value="{{ old('name') }}" required>
-                    <label for="name">Name</label>
-                    @error('name')
-                        <div class="invalid-feedback">
-                            {{ $message }}
-                        </div>
-                    @enderror
+<div class="row justify-content-center bg-light w-100 mx-0 min-vh-100 align-content-center">
+    <div class="col-md-4 bg-white rounded-4 shadow py-3 px-3">
+        <h1 class="h3 mb-3 fw-normal">Registrasi</h1>
+        <form action="/register" method="post">
+            @csrf
+            <input type="text" name="name" class="form-control rounded-4 mt-3 @error('name') is-invalid @enderror" id="name" placeholder="Nama" value="{{ old('name') }}" required>
+            @error('name')
+                <div class="invalid-feedback">
+                    {{ $message }}
                 </div>
-                <div class="form-floating">
-                    <input type="email" name="email" class="form-control @error('email') is-invalid @enderror" id="email" placeholder="name@example.com" value="{{ old('email') }}" required>
-                    <label for="email">Email</label>
-                    @error('email')
-                        <div class="invalid-feedback">
-                            {{ $message }}
-                        </div>
-                    @enderror
+            @enderror
+
+            <input type="email" name="email" class="form-control rounded-4 mt-3 @error('email') is-invalid @enderror" id="email" placeholder="Email" value="{{ old('email') }}" required>
+            @error('email')
+                <div class="invalid-feedback">
+                    {{ $message }}
                 </div>
-                <div class="form-floating">
-                    <input type="password" name="password" class="form-control rounded-bottom @error('password') is-invalid @enderror" id="password" placeholder="Password" required>
-                    <label for="password">Password</label>
-                    @error('password')
-                        <div class="invalid-feedback">
-                            {{ $message }}
-                        </div>
-                    @enderror
+            @enderror
+
+            <input type="password" name="password" class="form-control rounded-4 mt-3 @error('password') is-invalid @enderror" id="password" placeholder="Password" required>
+            @error('password')
+                <div class="invalid-feedback">
+                    {{ $message }}
                 </div>
-                <button class="w-100 btn btn-lg btn-primary mt-3" type="submit">Registed</button>
-            </form>
-            <small class="text-center d-block mt-3">Allready Registed? <a href="/login">Login</a></small>
-        </main>
+            @enderror
+
+            <button class="w-100 btn btn-lg btn-primary mt-3" type="submit">Registrasi</button>
+        </form>
+        <small class="text-center d-block mt-3">Sudah Punya Akun? <a href="/login" class="text-decoration-none">Login Sekarang</a></small>
     </div>
 </div>
 @endsection
