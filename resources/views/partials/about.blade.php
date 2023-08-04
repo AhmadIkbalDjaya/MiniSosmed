4cef9f42f819b67346f224e851a370b2a8669129
AhmadIkbalDjaya <ikbaldjaya@gmail.com>
2023-08-04 17:44:49 +0700

    copyr, test-dep

--- resources/views/partials/about.blade.php
+++ resources/views/partials/about.blade.php
@@ -2,5 +2,5 @@
     <a href="/about" class="text-black-50 text-decoration-none">About Me</a>
 </span>
 <span class="copyright text-black-50">
-    Ahmad Ikbal Djaya &copy 2022
+    &copy 2022, djaya_ikbal
 </span>
